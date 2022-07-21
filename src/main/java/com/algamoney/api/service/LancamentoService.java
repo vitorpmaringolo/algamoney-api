fff427d28d1de4f583ac5b62fce1febc6c2c2ece
Vitor Palhares Maringolo <vitor.pmaringolo@gmail.com>
2022-07-21 16:58:15 -0300

    5.6. Regra para não salvar pessoa inativa

---
+++ src/main/java/com/algamoney/api/service/LancamentoService.java
@@ -0,0 +1,31 @@
+package com.algamoney.api.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algamoney.api.model.Lancamento;
+import com.algamoney.api.model.Pessoa;
+import com.algamoney.api.repository.LancamentoRepository;
+import com.algamoney.api.repository.PessoaRepository;
+import com.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
+
+@Service
+public class LancamentoService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+
+	public Lancamento salvar(Lancamento lancamento) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+		if (pessoa.isEmpty() || pessoa.get().isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+		return lancamentoRepository.save(lancamento);
+	}
+
+}
